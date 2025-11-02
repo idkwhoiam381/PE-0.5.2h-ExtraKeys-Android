0790bb97bea1f4b9aac9a0230e6e1f78067380ec
idkwhoiam381 <ismotass86@gmail.com>
2025-11-02 15:00:49 +0300

    Add files via upload

--- source/FlxVideo.hx
+++ source/FlxVideo.hx
@@ -5,7 +5,7 @@ import openfl.events.NetStatusEvent;
 import openfl.media.Video;
 #elseif android
 import extension.webview.WebView;
-import android.AndroidTools;
+import android.*;
 #else
 import openfl.events.Event;
 import vlc.VlcBitmap;
@@ -48,15 +48,11 @@ class FlxVideo extends FlxBasic {
 			}
 		});
 		netStream.play(name);
+		#elseif android
 
-	        #elseif android
-                WebView.onClose=onClose;
-		        WebView.onURLChanging=onURLChanging;
-		        WebView.open(AndroidTools.getFileUrl(name));
-		        if (finishCallback != null){
-			        finishCallback();
-		        }
-               }
+		WebView.onClose=onClose;
+		WebView.onURLChanging=onURLChanging;
+		WebView.open(AndroidTools.getFileUrl(name));
 
 		#elseif desktop
 		// by Polybius, check out PolyEngine! https://github.com/polybiusproxy/PolyEngine
@@ -131,7 +127,6 @@ class FlxVideo extends FlxBasic {
 			finishCallback();
 		}
 	}
-
 	
 	function onVLCError()
 		{
@@ -140,5 +135,17 @@ class FlxVideo extends FlxBasic {
 				finishCallback();
 			}
 		}
+	#elseif android
+	function onClose() {
+		if (finishCallback != null){
+			finishCallback();
+		}
+	 }
+
+	function onURLChanging(url:String){
+		if (url == 'http://exitme/') 
+            onClose();
+	}
 	#end
 	#end
+}
